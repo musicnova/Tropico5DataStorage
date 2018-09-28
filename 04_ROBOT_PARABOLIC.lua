31b7df2e45c1a3d6f7626b8c7dbcd1c7894c7ca6
musicnova <musicnova@yandex.ru>
2018-09-28 17:39:29 +0300

    Create 04_ROBOT_PARABOLIC.lua

---
+++ 04_ROBOT_PARABOLIC.lua
@@ -0,0 +1,53 @@
+--РОБОТ ДЛЯ ТОРГОВЛИ НА БИРЖЕ НА СРОЧНОМ РЫНКЕ НА ОСНОВЕ ИНДИКАТОРА ПАРАБОЛИК
+--ВЕРСИЯ 4.0
+
+dofile(getScriptPath().."\\04_dll_RobotSAR.lua") -- файл с основными функциями
+
+FileLog = getScriptPath().."\\FileLog.txt"
+is_run = true
+Timer = 3
+Problem = ""
+count = 1
+DataTable = os.date("*t", os.time())
+
+--=====
+
+Class = "SBPFUT"
+Emit = "SiU7"
+MyAccount = "SBPFUT00a70"
+
+function OnInit()
+	--некие действия
+	TableID = AllocTable() -- будущая таблица робота
+	AddColumn(TableID,1,"ПАРАМЕТРЫ",true,QTABLE_STRING_TYPE,20)
+	AddColumn(TableID,2,"ЗНАЧЕНИЯ",true,QTABLE_STRING_TYPE,20)
+	AddColumn(TableID,3,"КОММЕНТАРИИ",true,QTABLE_STRING_TYPE,30)
+	CreateWindow(TableID)
+	PutDataToTableInit()
+	message("Робот запущен!",1)
+	WriteToEndOfFile(FileLog, "Робот запущен!")
+end
+
+function main()
+	while is_run==true do
+        Body()
+	end
+end
+
+function OnTrade(TradeX)
+	--действия при появлении новой сделки
+end
+
+function OnOrder(OrderX)
+	--действия при появлении заявки в КВИКе
+end
+
+function OnStopOrder()
+	--действия при появлении стоп-заявки
+end
+
+function OnStop()
+	--действия при нажатии на "остановить"
+	is_run = false
+	WriteToEndOfFile(FileLog, "Робот остановлен!")
+end
